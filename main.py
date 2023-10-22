c3f9711d35abf99871a7de5a0f443894dce090ee
cheuyin <yinstanleycheung@gmail.com>
2023-10-21 22:20:38 -0700

    Retrieve JSON data for an arbitrary post

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ import requests
 import json
 
 # Call API endpoint at https://hacker-news.firebaseio.com/v0/ and print the results
-response = requests.get("https://hacker-news.firebaseio.com/v0/")
-print(response)
+response = requests.get("https://hacker-news.firebaseio.com/v0/item/37958765.json?print=pretty")
+data = response.text
+parse_json = json.loads(data)
+
+print(parse_json['time'])
